5cf4146cbcf7ace586d3a4bfac98affeb6c96aac
richie <90250795+rishikalpadas@users.noreply.github.com>
2025-03-06 12:39:15 +0000

    discussed datatypes

--- test.js
+++ test.js
@@ -1,1 +1,42 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+const id = 2220;
+// console.log(id);
+
+// id = 2221;
+// console.log(id);
+
+let name = "rishi"
+// console.log(name);
+
+name = "payal"
+// console.log(name);
+{
+    let name = "payal"
+    // console.log(name);
+    
+}
+// console.log(name);
+
+var add = "kolkata"
+// console.log(add);
+
+var add = "naktala"
+// console.log(add);
+
+add = "kestopur"
+// console.log(add);
+
+
+// {
+    var add = "newtown"
+//     console.log(add);
+    
+// }
+
+// console.log(add);
+
+
+console.log("Chin Up Lad! You got this... :)")
+console.table(["apple","ball","cat"])
+console.table({firstname:"John", lastname:"Doe"})
