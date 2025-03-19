cec78ed005b189d1ee626711ced6f05592345aa4
richie <90250795+rishikalpadas@users.noreply.github.com>
2025-03-19 13:06:55 +0530

    declaring object literals and accessing the elements

---
+++ object.js
@@ -0,0 +1,14 @@
+// Object Literals
+
+const userDetails = {
+    name: "Rishi",
+    empId : 2217,
+    isLoggedIn : true,
+    last2LoginDays : ["Thursday", "Tuesday"]
+
+}
+
+// Accessing elements from the Object
+
+console.log(userDetails.name);
+console.log(userDetails["empId"]);
